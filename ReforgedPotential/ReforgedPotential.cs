d8382bf381e5a131f336c7657ab57d5487be7fdd
Akuichi <verusclues@gmail.com>
2026-09-21 13:53:47 +0800

    Bump plugin version and ignore package dir
    This change updates the plugin version from 2.0.1 to 2.0.2 and adds the generated /ReforgedPotential/Package directory to .gitignore so packaging artifacts are not tracked.

--- ReforgedPotential/ReforgedPotential.cs
+++ ReforgedPotential/ReforgedPotential.cs
@@ -33,7 +33,7 @@ namespace ReforgedPotential
     {
         public const string PluginGUID = "akuichi.ReforgedPotential";
         public const string PluginName = "Reforged Potential";
-        public const string PluginVersion = "2.0.1";
+        public const string PluginVersion = "2.0.2";
 
         
         private readonly Harmony harmony = new Harmony(PluginGUID);
