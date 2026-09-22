f126729a71d72daebc4ac788a8c0870df802979e
Akuichi <verusclues@gmail.com>
2026-09-22 22:37:20 +0800

    Update version to 2.0.4
    Update the Reforged Potential plugin version constant from 2.0.3 to 2.0.4 for the next release.

--- ReforgedPotential/ReforgedPotential.cs
+++ ReforgedPotential/ReforgedPotential.cs
@@ -33,7 +33,7 @@ namespace ReforgedPotential
     {
         public const string PluginGUID = "akuichi.ReforgedPotential";
         public const string PluginName = "Reforged Potential";
-        public const string PluginVersion = "2.0.3";
+        public const string PluginVersion = "2.0.4";
 
         
         private readonly Harmony harmony = new Harmony(PluginGUID);
